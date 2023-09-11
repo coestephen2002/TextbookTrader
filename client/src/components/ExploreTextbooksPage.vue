98ebe99ef6996adf80126d6bbb562063d9af5e02
coestephen2002 <66713914+coestephen2002@users.noreply.github.com>
2023-09-11 14:50:50 -0700

    added search bar to ExploreTextbooks page

--- client/src/components/ExploreTextbooksPage.vue
+++ client/src/components/ExploreTextbooksPage.vue
@@ -3,10 +3,18 @@
     <div class="textbooks-header">
       <h1>Explore Textbooks</h1>
       <br><br>
+      <div class="textbooks-fields">
+        <input type="text" v-model="searchText" placeholder="Search...">
+        <div class="buttons-container">
+          <button @click="searchTextbooks">Search</button>
+          <button @click="clearSearch">Clear</button>
+        </div>
+      </div>
+      <br><br>
     </div>
     <!-- List of Textbooks -->
-    <div class="textbooks-list border" v-for="textbook in getTextbooksWithUser" :key="textbook.id">
-      <div v-if="textbook.user_id !== getUserID">
+    <div v-for="textbook in getTextbooksWithUser" :key="textbook.id">
+      <div class="border" v-if="textbook.user_id !== getUserID && textbook.title.toLowerCase().includes(filterParam.toLowerCase())">
         <br>
         <label class="textbooks-label">Title: </label> <span> &nbsp;"{{ textbook.title }}"</span> <br>
         <label class="textbooks-label">ISBN: </label> <span> &nbsp;{{ textbook.isbn }}</span> <br>
@@ -25,6 +33,21 @@ export default {
   computed: {
     ...mapGetters('sessionManager', ['getUserID']),
     ...mapGetters('textbooksManager', ['getTextbooksWithUser'])
+  },
+  data() {
+    return {
+      searchText: '',
+      filterParam: ''
+    }
+  },
+  methods: {
+    searchTextbooks() {
+      this.filterParam = this.searchText
+    },
+    clearSearch() {
+      this.searchText = ''
+      this.filterParam = ''
+    }
   },
 }
 </script>
--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,15 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_08_13_233739) do
   create_table "jwt_denylist", force: :cascade do |t|
-    t.string "jti"
-    t.datetime "exp"
+    t.string "jti", null: false
+    t.datetime "exp", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
   end
 
   create_table "textbooks", force: :cascade do |t|
-    t.text "title"
+    t.string "title"
     t.text "isbn"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
